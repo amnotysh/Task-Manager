003334690509ff07074e4e70fa7ee011d3561969
unknown <amnotysh@gmail.com>
2025-08-05 18:12:48 +0800

    Update

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
 db = SQLAlchemy(app)
 
 class Todo(db.Model):
